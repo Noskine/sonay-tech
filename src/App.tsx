1e60e6d062dc07cbe7b58898a6464cd6afcb9dcb
Enikson Sonay <noskine@192-198-0-138.static.radiolinkinternet.com>
2024-01-03 12:18:01 -0300

    fix: creating the page readline

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,14 @@ function App() {
   return (
     <>
       <Header />
+      <section id="container-readline">
+        <div className="text">
+          <h1 className="gradient-green title">
+            Desenvolvedor Web Golang e Javascript
+          </h1>
+          <p className='subtitle'>Desenvolvimento Web: Criando aplicações performáticas, Conectando Empresas ao Mundo com Tecnologia</p>
+        </div>
+      </section>
     </>
   )
 }
