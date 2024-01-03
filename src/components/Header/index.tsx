344393f49ddbc7bc39d13976f282207359cec6a6
Enikson Sonay <noskine@192-198-0-138.static.radiolinkinternet.com>
2024-01-03 12:03:01 -0300

    fix: working with responsiveness, refactoring code and fixing errors

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,19 @@
 import './header.css'
 import logo from '../../assets/sonaytechlogo.svg'
-import { FaGithub } from "react-icons/fa";
+import { FaGithub, FaAlignJustify } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 
 export function Header() {
   return (
     <header>
       <section id="container-header">
-        <img src={logo} alt="logotipo sonay tech" id='logo-header' />
+        <section className='header-logotipo'>
+          <img src={logo} alt="logotipo sonay tech" id='logo-header' />
+          <section>
+            <p id='sonay' >Sonay</p>
+            <p id='webD'>Web developer</p>
+          </section>
+        </section>
         <nav>
           <ul>
             <li>
@@ -37,6 +43,9 @@ export function Header() {
             </li>
           </ul>
         </nav>
+        <button id='menu'>
+          <FaAlignJustify />
+        </button>
       </section>
     </header>
   );
