5974c7eb6bc853e6f029ed433a35e769f2da27e1
Enikson Sonay <noskine@192-198-0-138.static.radiolinkinternet.com>
2024-01-03 12:42:34 -0300

    fix: finishing the readline

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './app.css'
 import { Header } from './components/Header/index'
-
+import { FaGithub } from "react-icons/fa";
 function App() {
   return (
     <>
@@ -11,6 +11,16 @@ function App() {
             Desenvolvedor Web Golang e Javascript
           </h1>
           <p className='subtitle'>Desenvolvimento Web: Criando aplicações performáticas, Conectando Empresas ao Mundo com Tecnologia</p>
+          <section id="readline-buttons">
+            <button>
+              Entrar em Contato
+            </button>
+            <button className='github'>
+              <i>
+                <FaGithub />
+              </i> | Github
+            </button>
+          </section>
         </div>
       </section>
     </>
