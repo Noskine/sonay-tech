f51bbc427243727707d8a2e381ad57e3ac70d0bf
Enikson Sonay <noskine@192-198-0-138.static.radiolinkinternet.com>
2024-01-03 11:06:51 -0300

    fix: pilot commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './app.css'
+import { Header } from './components/Header/header'
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,43 @@
+import './header.css'
+import logo from '../../assets/sonaytechlogo.svg'
+import { FaGithub } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+
+export function Header() {
+  return (
+    <header>
+      <section id="container-header">
+        <img src={logo} alt="logotipo sonay tech" id='logo-header' />
+        <nav>
+          <ul>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Experience</a>
+            </li>
+            <li>
+              <a href="#">Projects</a>
+            </li>
+            <hr />
+            <li>
+              <a href="#">
+                <i>
+                  <FaGithub />
+                </i>
+              </a>
+            </li>
+            <hr />
+            <li>
+              <a href="#">
+                <i>
+                  <FaXTwitter />
+                </i>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </section>
+    </header>
+  );
+}
