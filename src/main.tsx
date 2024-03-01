0f37150c12d41b350086b5d4a28cdc51762e10aa
Noskine <noskine@outlook.com>
2024-03-01 01:47:25 -0300

    fix: set init-loading

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom/client'
 import Home from './pages/Home'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>,
-)
+setTimeout(() => {
+    ReactDOM.createRoot(document.getElementById('root')!).render(
+        <React.StrictMode>
+            <Home />
+        </React.StrictMode>,
+    )
+}, 2120)
